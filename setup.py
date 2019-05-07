64d729df86f429a8f2370e241eff917993b05a69
raidandfade <business@gocode.it>
2019-05-07 10:32:34 -0400

    Prepare PIP structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyCASC',  
+    version='0.0.1',
+    author="raidandfade",
+    author_email="business@gocode.it",
+    description="Pure Python CASC file structure reader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raidandfade/pycasc",
+    packages=["PyCASC","PyCASC.utils","PyCASC.utils.casc"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests==2.21.0",
+    ]
+ )
